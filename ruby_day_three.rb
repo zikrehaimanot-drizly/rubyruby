1212f876a0e635dbc80580d0aaeb294654f7a5c8
Zikre Haimanot <zikre@Zikres-MacBook-Pro.local>
2021-06-07 22:30:42 -0400

    day three

---
+++ ruby_day_three.rb
@@ -0,0 +1,6 @@
+// Create a function that takes 2 nonnegative integers in form of a string as an input, and outputs the sum (also as a string)
+
+def sum_str(a, b)
+ sum = a.to_i + b.to_i
+ sum.to_s
+end
