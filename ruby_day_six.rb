4f31461879d4926d322b83434eed22c99ae5a18f
Zikre Haimanot <zikre@zikres-mbp.myfiosgateway.com>
2021-06-10 20:35:31 -0400

    ZH [day six]

---
+++ ruby_day_six.rb
@@ -0,0 +1,11 @@
+//checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.//
+
+# //debug kata//
+
+def check_alive(health)
+  if 0 >= health
+    false
+  else
+    true
+  end
+end
