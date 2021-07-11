996621a19396469d2763a88c78dd9bff0d82afa8
Zikre Haimanot <zikre@zikres-mbp.myfiosgateway.com>
2021-07-10 21:18:37 -0400

    ZH [kata_count_by_x] :

---
+++ count_by_x.rb
@@ -0,0 +1,18 @@
+# Create a function with two arguments that will return an array of the first (n) multiples of (x).
+#
+# Assume both the given number and the number of times to count will be positive numbers greater than 0.
+#
+# Return the results as an array (or list in Python, Haskell or Elixir).
+#
+# Examples:
+
+
+def count_by(x, n)
+  arr = [x * n]
+  count = 1
+  while count < n do
+    arr.push(arr.last - x)
+    count += 1
+  end
+  p arr.reverse
+end
